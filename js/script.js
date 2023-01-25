3f1b0b8733cd442a31b75cfe102b3320f6c069ce
Atsuya Osaki <112054369+Atsuuuun@users.noreply.github.com>
2023-01-25 14:43:08 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $(function(){
+    var pagetop = $('#page-top');
+    pagetop.hide();
+    $(window).scroll(function () {
+       if ($(this).scrollTop() > 5) {
+            pagetop.fadeIn();
+       } else {
+            pagetop.fadeOut();
+       }
+    });
+    pagetop.click(function () {
+       $('body, html').animate({ scrollTop: 0 }, 500);
+       return false;
+    });
+  });
+
+});
