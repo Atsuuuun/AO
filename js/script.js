a90ed0cc80a9b8ca5977abb1adfc82f03387f814
Atsuya Osaki <112054369+Atsuuuun@users.noreply.github.com>
2023-01-31 14:25:22 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,20 +12,22 @@ $(function () {
   });
 
   //ページトップ
-  $(function(){
-    var pagetop = $('#page-top');
+  jQuery(function() {
+    var pagetop = $('#page_top');   
     pagetop.hide();
     $(window).scroll(function () {
-       if ($(this).scrollTop() > 5) {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
             pagetop.fadeIn();
-       } else {
+        } else {
             pagetop.fadeOut();
-       }
+        }
     });
     pagetop.click(function () {
-       $('body, html').animate({ scrollTop: 0 }, 500);
-       return false;
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
     });
-  });
+});
 
 });
